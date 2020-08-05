e9395d0d331a425ebe7c1060f09250a3be54cb07
chungiee <60273744+chungiee@users.noreply.github.com>
2020-08-05 13:43:14 -0400

    Add files via upload

---
+++ BST.cc
@@ -0,0 +1,277 @@
+#include <iostream>
+using namespace std;
+
+class TreeChunk {
+ public:
+  int data;
+  TreeChunk *left;
+  TreeChunk *right;
+
+  TreeChunk() {
+    data = 0;
+    left = right = NULL;
+  }
+};
+
+class BST {
+ public:
+  TreeChunk *root;
+
+  BST() {
+    root = NULL;
+  }
+
+  //1. Add new values to the tree
+  void add(int x) {
+    if(root == NULL) { //empty tree!
+      root = new TreeChunk;
+      root->data = x;
+      cout << "Just added value: " << x << endl;
+      return;
+    }
+
+    TreeChunk *slow, *fast; //slow= will NOT fall off tree, fast= will fall off, then gets added
+    slow = fast = root;
+
+    while(fast != NULL) {
+      if(x < fast->data) {
+	slow = fast;
+	fast = fast->left;
+      }
+      else if(x > fast->data) {
+	slow = fast;
+	fast = fast->right;
+      }
+      else {
+	cout << "We do not allow duplicates" << endl;
+	return;
+      }
+    }
+    
+    if(x < slow->data) {
+      slow->left = new TreeChunk;
+      slow->left->data = x;
+    }
+    else {
+      slow->right = new TreeChunk;
+      slow->right->data = x;
+    }
+    cout << "Successfully added: " << x << endl;
+  }
+  
+  //2. Search for a value
+  bool search(int key, TreeChunk *start) {
+    //anchor value
+    if(start == NULL) {
+      cout << "Could not find the value you are looking for" << endl;
+      return false;
+    }
+    //recursive step
+    else {
+	if(key == start->data) {
+	  cout << "Success! Value found in tree" << endl;
+	  return true;
+	}
+	if(key < start->data) {
+	  return search(key, start->left);
+	}
+	else {
+	  return search(key, start->right);
+	}
+    }	
+  }
+    
+  //3. Display contents
+  void preorder(TreeChunk *start) {
+    //anchor value
+    if(start == NULL)
+      return;
+    else {
+      //root, left, right
+      cout << start->data << endl;
+      preorder(start->left);
+      preorder(start->right);
+    }
+  }
+
+  void postorder(TreeChunk *start) {
+    //anchor value
+    if(start == NULL)
+      return;
+    else {
+      //left, right, root
+      postorder(start->left);
+      postorder(start->right);
+      cout << start->data << endl;
+    }
+  }
+
+  void inorder(TreeChunk *start) {
+    //anchor value
+    if(start == NULL)
+      return;
+    else {
+      //left, root, right
+      inorder(start->left);
+      cout << start->data << endl;
+      inorder(start->right);
+    }
+  }
+  
+  //4. Deleting values from the tree
+  void deleteNode(int key) {
+    //empty tree test
+    if(root == NULL) {
+      cout << "Empty tree, nothing to delete" << endl;
+      return;
+    }
+    
+    // 1) search and find that node
+    TreeChunk *slow, *fast;
+    slow = fast = root;
+
+    while(fast != NULL && fast->data != key) {
+      //keep going left or right
+      if(key < fast->data) {
+	slow = fast;
+	fast = fast->left;
+      }
+      else {
+	slow = fast;
+	fast = fast->right;
+      }
+    }
+    if(fast == NULL) {
+      cout << "This node does not exist in this tree" << key << endl;
+      return;
+    }
+
+    // 2) begin to classify the case for delete (1, 2, or 3)
+    cout << "Found the node, now trying to classify the type of delete!" << endl;
+    
+    //CASE 1: both the left and right points are NULL (No children)
+    if(fast->right == NULL && fast->left == NULL) {
+      cout << "Looks like a Case-I delete " << endl;
+      if(slow->right == fast) {
+	slow->right = NULL;
+	cout << "About to delete: " << fast->data << endl;
+	delete fast;
+      }
+      else {
+	//the node is to the left
+	slow->left = NULL;
+	cout << "About to delete: " << fast->data << endl;
+	delete fast;
+      }
+    }
+    //CASE 2: has ONE child
+    else if ((fast->right == NULL && fast->left != NULL) || (fast->right != NULL && fast->left == NULL)) {
+      cout << "Case II delete" << endl;
+      if(fast->left != NULL) {
+	if(slow->left == fast) {
+	  //fast is to the left of slow, and the lone child is to the left of fast
+	  slow->left = fast->left;
+	  cout << "About to delete: " << fast->data << endl;
+	  delete fast;
+	}
+	else if(slow->right == fast) {
+	  //fast is to the right of slow, and the lone child is to the left of fast
+	  slow->right = fast->left;
+	  cout << "About to delete: " << fast->data << endl;
+	  delete fast;
+	}
+      }
+      if(fast->right != NULL) {
+	if(slow->left == fast) {
+	  //fast is to the left of slow, and the lone child is to the right of fast
+	  slow->left = fast->right;
+	  cout << "About to delete: " << fast->data << endl;
+	  delete fast;
+	}
+        else if(slow->right == fast) {
+	  slow->right = fast->right;
+	  cout << "About to delete: " << fast->data << endl;
+	  delete fast;
+	}
+      }
+    }
+    // 3) proceed to delete
+    else {
+      cout << "This is a case III delete for " << key << endl;
+      TreeChunk *temp;
+      temp = fast->right;
+      while(temp->left != NULL) {
+	 temp = temp->left;
+      }
+      int key2 = temp->data;
+      deleteNode(key2);
+      fast->data = key2;
+      }
+    }
+
+};
+
+
+
+
+int main() {
+  BST myTree;
+  int choice1, choice2, x, key;
+
+  cout << "HELLOOO, welcome to Julia's Binary Search Tree." << endl;
+    
+  while(1) {
+    cout << " =======================" << endl;
+     cout << "Choose a number:" << endl;
+     cout << "1: ADD- Add new nodes to my tree!?" << endl;
+     cout << "2: SEARCH- Looking for a certain node in my tree?" << endl;
+     cout << "3: TRAVERSE- My tree is NOT empty so you can traverse it" << endl;
+     cout << "4: DELETE- Do you want to delete a node? :(" << endl;
+     cout << "ANYTHING ELSE TO QUIT!" << endl;
+     cin >> choice1;
+     
+    switch(choice1) {
+    case 1:
+      cout << "What number do you want to add?" << endl;
+      cin >> x;
+      cout << " =======================" << endl;
+      myTree.add(x);
+      break;
+    case 2:
+      cout << "What node are you searching for?" << endl;
+      cin >> key;
+      cout << " =======================" << endl;
+      myTree.search(key, myTree.root);
+      break;
+    case 3:
+      cout << "Choose a traversal:" << endl;
+      cout << "1: pre-order traversal" << endl;
+      cout << "2: post-order traversal" << endl;
+      cout << "3: in-order traversal" << endl;
+      cin >> choice2;
+      cout << " =======================" << endl;
+      
+      if (choice2 == 1) {
+	myTree.preorder(myTree.root);
+      }
+      else if (choice2 == 2) {
+	myTree.postorder(myTree.root);
+      }
+      else {
+	myTree.inorder(myTree.root);
+      }
+      break;
+    case 4:
+      cout << "Please input the value you want to delete: " << endl;
+      cin >> key;
+      cout << " =======================" << endl;
+      myTree.deleteNode(key);
+      break;
+    default:
+      cout << "BYE BYE" << endl;
+      exit(0);
+    }
+  }
+
+  return 0;
+}
---
+++ LL.cc
@@ -0,0 +1,164 @@
+#include <iostream>
+using namespace std;
+
+class Chunk {
+public:
+  int value;
+  Chunk *next;
+
+  Chunk() {
+    value = 0;
+    next = NULL;
+  }
+};
+
+class LinkedList {
+public:
+  Chunk *head;
+  int length;
+
+  LinkedList() {
+    head = NULL;
+    length = 0;
+  }
+  
+ 
+  
+  void insertAtHead(int value) {
+    //insert at position 1
+    Chunk *temp = new Chunk;
+    temp->value = value;
+    
+    temp->next = head;
+    head = temp;
+    length++;
+  }
+  
+  //1. Add something to the list
+  void insertAtPosition(int position, int value) {
+    //check if position is valid
+    if (position > 0 && position <= length + 1) {
+      Chunk *temp = new Chunk;
+      temp->value = value;
+
+      if(position == 1) {
+	temp->next = head;
+	head = temp;
+      }
+      else if(position > 1 && position <= length) {
+	Chunk *helper = head;
+	//take helper to (position - 1)
+	for(int i = 1; i < position - 1; i++)
+	{
+	  helper = helper->next;
+	  cout << "Helper @: " << helper->value << endl; //prints out where helper is exactly
+	}
+	
+	temp->next = helper->next;
+	helper->next = temp;	
+      }
+      else {
+	Chunk *helper = head;
+	while(helper->next != NULL)
+	    helper = helper->next;
+        helper->next = temp;
+      }
+      length++; //DO NOT FORGET
+    }
+    else
+    {
+      cout << "Can't do that, invalid position" << endl;
+    }
+  }
+  
+
+  
+  //2. Remove something from the list
+  void removeFromPosition(int position) {
+    if(position > 0 && position <= length+1) {
+      if(position == 1 && length == 1) {
+	delete head;
+	head = NULL;
+      }
+      else if(position == 1 && length > 1) {
+	Chunk *temp = head;
+	head = head->next;
+	delete temp;
+      }
+      else if(position > 1 && position <= length)
+	{
+	  Chunk *temp, *slow;
+	  temp = slow = head;
+
+	  for(int i = 1; i < position; i++) {
+	    slow = temp;
+	    temp = temp->next; //makes sure temp is always in front of slow
+	  }
+	  slow->next = temp->next;
+	  delete temp;
+	}
+      length--;
+    }
+    else
+    {
+      cout << "Will not delete: invalid position" << endl;
+    }
+  }
+
+  //3. Display contents
+  void displayContents() {
+    if(head==NULL) {
+      cout << "Empty list, nothing to display" << endl;
+    }
+    else {
+      Chunk *temp;
+      temp = head;
+      cout << "head-->";
+      while (temp != NULL) {
+	cout << temp->value << "-->";
+	temp = temp->next;
+      }
+      cout << "NULL" << endl;
+    }
+  }
+};
+
+int main() {
+  LinkedList LL;
+  int choice, value, position;
+
+  LL.displayContents();
+
+  while(1) {
+    cout << "Press 1 to insert at head" << endl;
+    cout << "Press 2 to insert at specific position" << endl;
+    cout << "Press 3 to remove from specific position" << endl;
+    cout << "Press 4 to display" << endl;
+    cout << "Anything else to quit" << endl;
+    cin >> choice;
+
+    switch(choice) {
+    case 1: cout << "Insert what?" << endl;
+      cin >> value;
+      LL.insertAtHead(value);
+      break;
+    case 2: cout << "Insert what?" << endl;
+      cin >> value;
+      cout << "Insert where?" << endl;
+      cin >> position;
+      LL.insertAtPosition(position, value);
+      break;
+    case 3: cout << "Remove from where?" << endl;
+      cin >> position;
+      LL.removeFromPosition(position);
+      break;
+    case 4: cout << "-----------" << endl;
+      LL.displayContents();
+      cout << "-----------" << endl;
+      break;
+    default: cout << "Goodbye!" << endl;
+      exit(1);
+    }
+  }
+  
+}
